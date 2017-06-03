ac78ad047420f1e24963075e8263d6f9e76643c9
davidbgonz <david.gonz789@gmail.com>
2017-06-03 14:01:32 -0400

    Added variables to be passed to page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var colors = [
+    "Black", "Blue", "Brown", "Cork", "Cream", "Gold", "Green", "Misc", "Orange", "Purple", "Red", "Silver", "White", "Yellow"
+]
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title: 'Bottlecap Archive',
+    colors: colors
+  });
 });
 
 module.exports = router;
