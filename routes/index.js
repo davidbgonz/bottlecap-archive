29903272ff375d756c966c53084d7c0aa77d33ba
davidbgonz <davidbgonz@users.noreply.github.com>
2017-07-15 21:06:19 -0400

    Removed excess code

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,6 @@ var express = require('express');
 var router = express.Router();
 var capStats = require('../public/javascripts/colorClass');
 
-var colors = [
-    "Black", "Blue", "Brown", "Cork", "Cream", "Gold", "Green", "Misc", "Orange", "Purple", "Red", "Silver", "White", "Yellow"
-];
-
-  console.log(capStats);
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
